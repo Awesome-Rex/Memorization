e217728ca32c83a08a89cda3a0a1fadce1c784f9
Awesome-Rex <john.esqqq@gmail.com>
2020-03-10 22:13:50 -0400

    decided to just keep namefield in same location

--- Taekwondo Practice/Resources/Views/UserControls/SettingFields/NameField .xaml.cs
+++ Taekwondo Practice/Resources/Views/UserControls/SettingFields/NameField .xaml.cs
@@ -21,8 +21,6 @@ namespace Practice_Dojang.Resources.Scripts.Views.UserControls.SettingFields
             get { return (String)GetValue(FieldNameProperty); }
             set { SetValue(FieldNameProperty, value); }
         }
-
-        // Using a DependencyProperty as the backing store for label.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty FieldNameProperty = DependencyProperty.Register("FieldName", typeof(String), typeof(NameField), new PropertyMetadata("New LineField"));
 
 
@@ -32,15 +30,18 @@ namespace Practice_Dojang.Resources.Scripts.Views.UserControls.SettingFields
             get { return (UIElement)GetValue(FieldProperty); }
             set { SetValue(FieldProperty, value); }
         }
-
-        // Using a DependencyProperty as the backing store for combo.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty FieldProperty = DependencyProperty.Register("Field", typeof(UIElement), typeof(NameField), new PropertyMetadata(null));
+
 
         public GridLength NameWidth { get { return (GridLength)GetValue(NameWidthProperty); } set { SetValue(NameWidthProperty, value); } }
         public static readonly DependencyProperty NameWidthProperty = DependencyProperty.Register("NameWidth", typeof(GridLength), typeof(NameField), new PropertyMetadata(new GridLength(1, GridUnitType.Star)));
 
         public GridLength FieldWidth { get { return (GridLength)GetValue(FieldWidthProperty); } set { SetValue(FieldWidthProperty, value); } }
         public static readonly DependencyProperty FieldWidthProperty = DependencyProperty.Register("FieldWidth", typeof(GridLength), typeof(NameField), new PropertyMetadata(new GridLength(2, GridUnitType.Star)));
+
+
+        public bool FieldOverflow { get { return (bool)GetValue(FieldOverflowProperty); } set { SetValue(FieldOverflowProperty, value); } }
+        public static readonly DependencyProperty FieldOverflowProperty = DependencyProperty.Register("FieldOverflow", typeof(bool), typeof(NameField), new PropertyMetadata(false));
 
         public NameField()
         {
